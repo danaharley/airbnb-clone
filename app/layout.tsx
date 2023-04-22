a7d89e8daae89fb0ea58a806ad9eb9e317744eb6
danaharley <danaharley.github@gmail.com>
2023-04-22 19:44:18 +0700

    auth ui -> register

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Nunito } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./provider/ToasterProvider";
 // import ClientOnly from "./components/ClientOnly";
 
 export const metadata = {
@@ -22,6 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         {/* <ClientOnly> */}
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {/* </ClientOnly> */}
         {children}

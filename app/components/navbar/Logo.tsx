d083cd884ead3da7361cebff03838d75358454c2
danaharley <danaharley.github@gmail.com>
2023-04-22 09:57:58 +0700

    navbar ui

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+
+const Logo = () => {
+  return (
+    <Image
+      src="/images/logo.png"
+      alt="logo"
+      height={100}
+      width={100}
+      className="hidden md:block cursor-pointer"
+    />
+  );
+};
+
+export default Logo;

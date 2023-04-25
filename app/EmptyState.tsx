62a161bfca0bba7351b30982caa322ae67e71b21
danaharley <danaharley.github@gmail.com>
2023-04-25 21:50:18 +0700

    fetching listing server component

--- app/EmptyState.tsx
+++ app/EmptyState.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
 
-const EmptyState = () => {
-  return <div>EmptyState</div>;
+import { useRouter } from "next/navigation";
+import React from "react";
+import Heading from "./components/Heading";
+import Button from "./components/Button";
+
+interface EmptyStateProps {
+  title?: string;
+  subtitle?: string;
+  showReset?: boolean;
+}
+
+const EmptyState: React.FC<EmptyStateProps> = ({
+  title = "No exact matches",
+  subtitle = "Try changing or removing some of your filters",
+  showReset,
+}) => {
+  const router = useRouter();
+
+  return (
+    <div className="h-[60vh] flex flex-col items-center justify-center gap-2">
+      <Heading center title={title} subtitle={subtitle} />
+      <div className="w-48 mt-4">
+        {showReset && (
+          <Button
+            outline
+            label="Remove all filters"
+            onClick={() => router.push("/")}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default EmptyState;
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Container from "./components/Container";
 import EmptyState from "./EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listing/ListingCard";
+import ClientOnly from "./components/ClientOnly";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
 
-  if (isEmpty) {
-    return <EmptyState />;
+  if (!listings.length) {
+    return <EmptyState showReset />;
   }
 
   return (
-    <Container>
-      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        <div>Dana</div>
-      </div>
-    </Container>
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing) => (
+            <ListingCard key={listing.id} data={listing} />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   );
 }
